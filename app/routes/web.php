1444177fe72149e852966a9cb30d8d60a35a768e
mikan-810 <2401320006nn@cyber-u.ac.jp>
2024-11-09 08:46:59 +0900

    todoの情報を返すコントローラをカスタマイズする

--- app/routes/web.php
+++ app/routes/web.php
@@ -24,3 +24,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('todo', 'App\Http\Controllers\TodoController');
+Route::put('todo/done/{todo}', [App\Http\Controllers\TodoController::class, 'done'])->name('todo.done');
+Route::put('todo/undone/{todo}', [App\Http\Controllers\TodoController::class, 'undone'])->name('todo.undone');
